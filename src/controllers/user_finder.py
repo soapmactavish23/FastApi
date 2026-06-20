b445ebcf452969ee03e1efc1f27ca000e4abff87
soapmactavish23 <ricknogueira1231@gmail.com>
2026-06-20 20:25:18 -0300

    Definindo Lógicas de Inserção e Busca de Usuários

---
+++ src/controllers/user_finder.py
@@ -0,0 +1,14 @@
+from src.models.repositories.interfaces.users_repository_interface import UsersRepositoryInterface
+
+
+class UserFinder:
+    def __init__(self, users_repository: UsersRepositoryInterface) -> None:
+        self.__users_repository = users_repository
+
+    async def find_user_by_name(self, user_name: str) -> dict:
+        users = await self.__users_repository.get_users_by_name(user_name)
+        return {
+            "type": "USERS",
+            "count": len(users),
+            "attributes": users
+        }
--- src/controllers/user_register.py
+++ src/controllers/user_register.py
@@ -2,5 +2,30 @@ from src.models.repositories.interfaces.users_repository_interface import UsersR
 
 
 class UserRegister:
-    def __init__(self, users_repository: UsersRepositoryInterface):
-        self.users_repository = users_repository
+    def __init__(self, users_repository: UsersRepositoryInterface) -> None:
+        self.__users_repository = users_repository
+
+    async def register_user(self, user_data: dict) -> dict:
+        self.__validate_user_data(user_data)
+        await self.__register_user(user_data)
+        return self.__format_response(user_data)
+
+    def __validate_user_data(self, user_data: dict) -> None:
+        age = user_data['age']
+        uf = user_data['uf'].upper()
+
+        if uf not in ["MG", "PA", "BA", "CE", "SC", "MT"]:
+            raise Exception("Estado invalido para cadastro")
+
+        if age < 0 or age > 120:
+            raise Exception("Idade invalida para cadastro")
+
+    async def __register_user(self, user_data: dict) -> None:
+        await self.__users_repository.insert_users(user_data)
+
+    def __format_response(self, user_data: dict) -> dict:
+        return {
+            "type": "USERS",
+            "count": 1,
+            "attributes": user_data
+        }
